9e6971c27602f0ac24e33cda087592594f493783
Rayyan Salman <ryaansalman@gmail.com>
2024-07-28 03:47:18 +0500

    Complete Whisper Project

---
+++ Whisper/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import axios from "axios"
+
+const app = express();
+const port=3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req,res)=>{
+    const result = await axios.get("https://secrets-api.appbrewery.com/random");
+    console.log(result.data);
+    res.render("index.ejs",{secret : result.data.secret,user:result.data.username});
+})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
