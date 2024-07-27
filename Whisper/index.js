8f88fc0340b3926608828ee8702a9b61ad200d3b
Rayyan Salman <ryaansalman@gmail.com>
2024-07-28 04:12:09 +0500

    added try catch statement to get '/' endpoint

--- Whisper/index.js
+++ Whisper/index.js
@@ -7,9 +7,14 @@ const port=3000;
 app.use(express.static("public"));
 
 app.get("/", async (req,res)=>{
+    try{
     const result = await axios.get("https://secrets-api.appbrewery.com/random");
     console.log(result.data);
     res.render("index.ejs",{secret : result.data.secret,user:result.data.username});
+    }catch(error){
+        console.log(error.response.data);
+        res.status(500);
+    }
 })
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
