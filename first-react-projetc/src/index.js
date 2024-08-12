f6755f239db55813692cd7478b66bfb1c7413fdc
Rayyan Salman <ryaansalman@gmail.com>
2024-08-12 07:12:12 +0500

    Complete First react roject

---
+++ first-react-projetc/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Why i hate Fish</h1>
+    <ul>
+      <li>They just go blub blub</li>
+      <li>They die if you don't have water</li>
+      <li>They look weird</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
