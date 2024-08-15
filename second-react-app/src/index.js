337b4c2b07d69d6b09d1ef25b1cb74d4f1251651
Rayyan Salman <ryaansalman@gmail.com>
2024-08-15 12:54:54 +0500

    Fix Injection Issue

--- second-react-app/src/index.js
+++ second-react-app/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Heading from "./heading";
+import Heading from "./heading.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Heading />);
+ReactDOM.createRoot(document.getElementById("root")).render(Heading());
