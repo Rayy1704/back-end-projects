31ac9897792c4ee7b4ad863a4796e48b2a5e851d
Rayyan Salman <ryaansalman@gmail.com>
2024-08-16 16:44:48 +0500

    Fix bug-Missing dependencies

--- keeper-app/src/index.jsx
+++ keeper-app/src/index.jsx
@@ -1,7 +1,7 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 import React from "react";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <App />
+    <App/>
 )
