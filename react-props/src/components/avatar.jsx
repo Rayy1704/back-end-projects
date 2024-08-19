37340d5fff0a8a610168a1b7489aabd01dc8b9bb
Rayyan Salman <ryaansalman@gmail.com>
2024-08-19 15:15:38 +0500

    Split into further components

---
+++ react-props/src/components/avatar.jsx
@@ -0,0 +1,10 @@
+
+function Img(prop){
+    return (<img
+        src={prop.src}
+        alt="avatar_img"
+        className="circle-img"
+    />)
+}
+
+export default Img;
--- react-props/src/components/card.jsx
+++ react-props/src/components/card.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-
+import Img from "./avatar";
 function Card(prop) {
   return (
     
       <div className="card">
         <div className="top">
           <h2 className="name">{prop.name}</h2>
-          <img
-            src={prop.src}
-            alt="avatar_img"
-            className="circle-img"
-          />
+          <Img src ={prop.src}/>
         </div>
         <div className="bottom">
           <p className="info">{prop.pn}</p>
