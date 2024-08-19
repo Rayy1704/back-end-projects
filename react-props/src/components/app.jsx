a8518900a51e8ae17198ef53a83cbd988e00b3f3
Rayyan Salman <ryaansalman@gmail.com>
2024-08-19 16:17:47 +0500

    Clean up App.jsx

--- react-props/src/components/app.jsx
+++ react-props/src/components/app.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import Card from "./card"; 
 import contacts from "../contacts"; 
+import Avatar from "./avatar";
+
+function createCard(contact){
+  return<Card key={contact.id} name={contact.name} src={contact.imgURL} pn={contact.phone} email={contact.email}/>
+}
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      {contacts.map((i) => (
-        <Card 
-          key={i.id} 
-          name={i.name} 
-          src={i.imgURL} 
-          pn={i.phone} 
-          email={i.email} 
-        />
+      <Avatar src = "https://media.licdn.com/dms/image/v2/D4D03AQF5e-9Ge1WVyw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1722952271199?e=1729728000&v=beta&t=XSFSj3V6Kt-5ENYflKO2u418SqnyrTO0GfJP-tpSOb8"/>
+      {contacts.map((contact) => (
+        createCard(contact)
       ))}
     </div>
   );
