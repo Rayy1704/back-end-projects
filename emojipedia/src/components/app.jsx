ac1ed38b3023a39bfd9794648dfa8d43ff0a25f2
Rayyan Salman <ryaansalman@gmail.com>
2024-08-20 20:22:04 +0500

    Add complexity to code

--- emojipedia/src/components/app.jsx
+++ emojipedia/src/components/app.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import Card from "./card";
 import emojipedia from "../emojipedia";
-function createCard(emoji){
-    return(
-        <div className="term"><Card key={emoji.id} emoji={emoji.emoji} meaning={emoji.meaning} name = {emoji.name}/></div>
-    )
-}
 
 function App(){
     return(
         <div>
             <h1><span>EmojiPedia</span></h1>
             <dl className="dictionary">
-                {emojipedia.map((emoji)=>(createCard(emoji)))}
+                {emojipedia.map(emoji=><div className="term"><Card key={emoji.id} emoji={emoji.emoji} meaning={emoji.meaning} name = {emoji.name}/></div>)}
             </dl>
         </div>
     );
