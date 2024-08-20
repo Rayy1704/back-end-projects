a7b4ef6bb2a1f8cdf2fb4e9254e9592789ed0346
Rayyan Salman <ryaansalman@gmail.com>
2024-08-20 19:09:53 +0500

    Add Ids to contacs

--- react-props/src/contacts.js
+++ react-props/src/contacts.js
@@ -1,11 +1,13 @@
 const contacts = [
     {
+      id:1,
       name: "Beyonce",
       imgURL:
         "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
       phone: "+123 456 789",
       email: "b@beyonce.com"
     },{
+      id:2,
       name: "Chuck Norris",
       imgURL:
         "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
@@ -13,6 +15,7 @@ const contacts = [
       email: "gmail@chucknorris.com"
     },
     {
+      id:3,
       name: "Jack Bauer",
       imgURL:
         "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
