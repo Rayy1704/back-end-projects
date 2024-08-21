30b70bc91d7db9acb1d90060e1b1b8ed1823925b
Rayyan Salman <ryaansalman@gmail.com>
2024-08-21 17:37:25 +0500

    Finish automatic clock

--- 307-useState-hook-practice/src/components/App.jsx
+++ 307-useState-hook-practice/src/components/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-
+function getTime(){
+  return new Date().toLocaleTimeString().substring(0,7);
+}
 function App() {
+  let [time,setTime] = React.useState(getTime()) ;
+  function displayTime(){
+    setTime(getTime());
+  }
+  setInterval(displayTime,1000);
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{time}</h1>
     </div>
   );
 }
