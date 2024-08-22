89770d2a2b56225957f801b94c9fc7c7758d583d
Rayyan Salman <ryaansalman@gmail.com>
2024-08-22 15:38:59 +0500

    Completed Animated Form Component using hooks

--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React,{useState} from "react";
 
 function App() {
+  const [isHovered,setHovered] = useState(false);
+  const [headingText,setHeading]= useState("Hello")
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?"/>
+      <button onClick={()=>{setHeading("Submitted")}} style = {{backgroundColor:(isHovered?'black':'white'),}} onMouseEnter={()=>{setHovered(true)}} onMouseLeave={()=>{setHovered(false)}}>Submit</button>
     </div>
   );
 }
