e70b022f957efc38eca238f3105766ce620ca675
Rayyan Salman <ryaansalman@gmail.com>
2024-08-25 14:58:19 +0500

    Add Dynamic name functionality

--- react-forms/src/components/App.jsx
+++ react-forms/src/components/App.jsx
@@ -2,12 +2,26 @@ import React,{useState} from "react";
 
 function App() {
   const [isHovered,setHovered] = useState(false);
-  const [headingText,setHeading]= useState("Hello")
+  const [headingText,setHeading]= useState("Hello");
+  const [name,setName]=useState("");
+  function handleChange(event){
+    setName(event.target.value);
+  }
   return (
     <div className="container">
       <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?"/>
-      <button onClick={()=>{setHeading("Submitted")}} style = {{backgroundColor:(isHovered?'black':'white'),}} onMouseEnter={()=>{setHovered(true)}} onMouseLeave={()=>{setHovered(false)}}>Submit</button>
+      <input 
+        className ="nameBox" 
+        type="text" 
+        placeholder="What's your name?" 
+        onChange ={handleChange}
+      />
+      <button 
+        onClick={()=>{setHeading(`Hello, ${name}`)}} 
+        style = {{backgroundColor:(isHovered?'black':'white'),}} 
+        onMouseEnter={()=>{setHovered(true)}} 
+        onMouseLeave={()=>{setHovered(false)}}
+      >Submit</button>
     </div>
   );
 }
